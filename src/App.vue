66b164e93223be8ec2a8a2e078bc2d4c88b77507
Evgenii Lepekho <lepehun@mail.ru>
2024-05-05 14:29:44 +0200

    feat: install axios, change favicon, add fetch items with search and sort

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,47 @@
 <script setup>
+import { onMounted, reactive, ref, watch } from 'vue'
+import axios from 'axios'
+
 import Header from './components/Header.vue'
 import CardList from './components/CardList.vue'
 import Drawer from './components/Drawer.vue'
+
+const items = ref([])
+
+const filters = reactive({
+  sortBy: 'title',
+  searchQuery: ''
+})
+
+const onChangeSelect = (event) => {
+  filters.sortBy = event.target.value
+}
+
+const onChangeSearchInput = (event) => {
+  filters.searchQuery = event.target.value
+}
+
+const fetchItems = async () => {
+  try {
+    const params = {
+      sortBy: filters.sortBy
+    }
+
+    if (filters.searchQuery) {
+      params.title = `*${filters.searchQuery}*`
+    }
+
+    const { data } = await axios.get('https://e0df4bb822e07583.mokky.dev/items', { params })
+
+    items.value = data
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+onMounted(fetchItems)
+
+watch(filters, fetchItems)
 </script>
 
 <template>
@@ -9,11 +49,31 @@ import Drawer from './components/Drawer.vue'
     <Header />
 
     <div class="p-10">
-      <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
-      <CardList />
+      <div class="flex justify-between items-center mb-8">
+        <h2 class="text-3xl font-bold">Все кроссовки</h2>
+        <div class="flex gap-4">
+          <select @change="onChangeSelect" class="py-2 px-4 border rounded outline-none">
+            <option value="name">По названию</option>
+            <option value="price">По цене (дешевые)</option>
+            <option value="-price">По цене (дорогие)</option>
+          </select>
+          <div class="relative">
+            <img src="/search.svg" alt="search image" class="absolute top-3 left-4" />
+            <input
+              @input="onChangeSearchInput"
+              type="text"
+              placeholder="Поиск..."
+              class="border rounded-xl py-2 pl-12 pr-4 outline-none focus:border-gray-400"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="mt-10">
+        <CardList :items="items" />
+      </div>
     </div>
   </div>
-  <Drawer />
+  <!-- <Drawer /> -->
 </template>
 
 <style scoped></style>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,6 +1,10 @@
 <script setup>
 import Card from './Card.vue'
 
+defineProps({
+  items: Array
+})
+
 const onClickAdd = () => {
   alert('Добавлено')
 }
@@ -9,11 +13,11 @@ const onClickAdd = () => {
 <template>
   <div class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
     <Card
-      title="Мужские кроссовки"
-      imageUrl="/sneakers/sneakers-1.jpg"
-      :price="5000"
-      isFavorite
-      isAdded
+      v-for="item in items"
+      :key="item.id"
+      :title="item.title"
+      :imageUrl="item.imageUrl"
+      :price="item.price"
       :onClickAdd="onClickAdd"
     />
   </div>
